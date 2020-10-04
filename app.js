778c082103c0b417773220fc77ac42daec4b7fb3
piotrmaz <maximus_decimus@o2.pl>
2020-10-04 23:17:37 +0200

    customize design advanced statistic section

--- app.js
+++ app.js
@@ -6,48 +6,42 @@ const fetch = require('node-fetch');
 const app = express();
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.render("shortening", {
     oldUrl: oldUrl,
-      newUrl: newUrl
+    newUrl: newUrl
   });
 });
 
 let oldUrl = [];
-let newUrl=[];
-app.post("/", function(req, res){
-let incoming = req.body.oldUrl;
+let newUrl = [];
+app.post("/", function(req, res) {
+  let incoming = req.body.oldUrl;
   fetch("https://rel.ink/api/links/", {
-                method: 'POST',
-                body: JSON.stringify({
+      method: 'POST',
+      body: JSON.stringify({
         url: incoming
       }),
-                headers: {
-                         'Content-type': 'application/json'
-                     },
-            })
-            .then(res => res.json())
-            .then(json => {
-                let newUrlLink = "https://rel.ink/" + json.hashid;
-                let oldUrlLink = json.url;
-                //console.log("nowy url = " + json.url);
-oldUrl.push(oldUrlLink);
-newUrl.push(newUrlLink);
-res.render("shortening", {
-  oldUrl: oldUrl,
-  newUrl: newUrl
-});
-            });
-
-
-  //console.log(incoming);
-
-
+      headers: {
+        'Content-type': 'application/json'
+      },
+    })
+    .then(res => res.json())
+    .then(json => {
+      let newUrlLink = "https://rel.ink/" + json.hashid;
+      let oldUrlLink = json.url;
+      //console.log("nowy url = " + json.url);
+      oldUrl.push(oldUrlLink);
+      newUrl.push(newUrlLink);
+      res.redirect("/");
+    });
 });
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
